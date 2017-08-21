b0a68f85a1c17abec95e32d437014d2110da5788
Max Verigin <maxverigin@gmail.com>
2017-08-20 21:06:52 -0700

    create database working

--- gather/gatherer.go
+++ gather/gatherer.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	// "errors"
-	log "github.com/sirupsen/logrus"
+	"database/sql"
+	"flag"
+	"fmt"
 	"net"
 	"net/http"
 	"net/rpc"
 
+	log "github.com/sirupsen/logrus"
+
+	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/griever989/gatherlogs/common"
 	"github.com/mattn/go-colorable"
 )
@@ -27,7 +31,7 @@ func NewGatherer() *Gatherer {
 
 // Send sends a log message
 func (g *Gatherer) Send(msg common.LogMessage, reply *bool) error {
-	log.Debug("send called with", msg)
+	log.Debug("send called with ", msg)
 	g.receiveQueue <- msg
 	log.Debug("message written to queue")
 	return nil
@@ -35,7 +39,7 @@ func (g *Gatherer) Send(msg common.LogMessage, reply *bool) error {
 
 // SendMultiple sends many log messages
 func (g *Gatherer) SendMultiple(msgs []common.LogMessage, reply *bool) error {
-	log.Debug("send called with", msgs)
+	log.Debug("send called with ", msgs)
 	for _, msg := range msgs {
 		g.receiveQueue <- msg
 	}
@@ -57,10 +61,72 @@ func serve() *Gatherer {
 	return g
 }
 
-func consume(g *Gatherer) {
+func consumeCli(g *Gatherer) {
+	log.Info("writing queue to command-line")
 	for {
 		log.Debug("consumed queue message ", <-g.receiveQueue)
 	}
+}
+
+func consumeDatabase(g *Gatherer, driver, connectionString, dbSchema, dbTable string, dbCreate bool) {
+	db, err := sql.Open(driver, connectionString)
+	if err != nil {
+		log.Fatalln("failed to open db", driver, connectionString, err)
+	}
+	if !dbExists(db, dbSchema, dbTable) {
+		if !dbCreate {
+			log.Fatalln("database", dbSchema, dbTable, "does not exist and -dbCreate is not set. Provide -dbSchema and -dbTable as well as -dbCreate if you want to create the table automatically")
+		}
+		createDbTable(db, dbSchema, dbTable)
+	}
+	log.Info("writing queue to database")
+	for {
+		msg := <-g.receiveQueue
+		_, err := db.Exec(`
+			insert into [$1].[$2] (Server, LogLevel, Time, Message)
+			values ($3, $4, $5, $6);`,
+			dbSchema, dbTable,
+			msg.GetServer(), msg.GetLogLevel(), msg.GetTime(), msg.GetMessage())
+		if err != nil {
+			log.Warnln("Failed to insert message", msg, err)
+		}
+	}
+}
+
+func dbExists(db *sql.DB, dbSchema, dbTable string) bool {
+	rows, err := db.Query("select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = $1 and TABLE_NAME = $2", dbSchema, dbTable)
+	if err != nil {
+		log.Fatalln("failed to check for existence of", dbSchema, dbTable, err)
+	}
+	if rows.Next() {
+		return true
+	}
+	return false
+}
+
+func createDbTable(db *sql.DB, dbSchema, dbTable string) {
+	fullTable := fmt.Sprintf("[%v].[%v]", dbSchema, dbTable)
+	_, err := db.Exec(`
+		create table ` + fullTable + `
+		(
+			Id bigint NOT NULL IDENTITY (1,1) PRIMARY KEY NONCLUSTERED,
+			Server nvarchar(255) NOT NULL,
+			LogLevel nvarchar(255) NOT NULL,
+			Time datetime2 NOT NULL,
+			Message nvarchar(max) NULL
+		);`)
+	if err != nil {
+		log.Fatalln("failed to create table", fullTable, err)
+	}
+	log.Infoln("created table", dbSchema, dbTable)
+
+	_, err = db.Exec(
+		"create clustered index IX_primary on " + fullTable + " (Time, Id);")
+
+	if err != nil {
+		log.Fatalln("failed to create index", dbSchema, dbTable, err)
+	}
+	log.Infoln("created index", dbSchema, dbTable)
 }
 
 func configLogger() {
@@ -70,7 +136,25 @@ func configLogger() {
 }
 
 func main() {
+	consumerType := flag.String("consumer", "cli", "-consumer=( cli | database )")
+	connectionString := flag.String("connectionString", "", "-connectionString=<connection-string>")
+	dbDriver := flag.String("driver", "mssql", "-driver=( mssql | <driver-name> ) -- If the driver name you want is not present, you can provide it, but you need to 'go get' the package yourself on the machine running it")
+	dbSchema := flag.String("schema", "dbo", "-schema=<db-schema-name>")
+	dbTable := flag.String("table", "gatherer", "-table=<db-table-name>")
+	dbCreate := flag.Bool("dbCreate", false, "-dbCreate")
+
+	flag.Parse()
+
 	configLogger()
 	g := serve()
-	consume(g)
+
+	switch *consumerType {
+	case "cli":
+		consumeCli(g)
+	case "database":
+		consumeDatabase(g, *dbDriver, *connectionString, *dbSchema, *dbTable, *dbCreate)
+	default:
+		log.Fatal("unrecognized consumer value ", *consumerType)
+	}
+
 }
