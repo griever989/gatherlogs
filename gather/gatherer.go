16ca09076d66b87817f3c7e249ab6970e83e059f
Max Verigin <maxverigin@gmail.com>
2017-08-20 17:44:17 -0700

    sending multiple messages at once

--- gather/gatherer.go
+++ gather/gatherer.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	// "errors"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net"
 	"net/http"
 	"net/rpc"
 
 	"github.com/griever989/gatherlogs/common"
+	"github.com/mattn/go-colorable"
 )
 
 var protocol = "tcp"
 var port = ":9494"
+var channelBufferSize = 1
 
 // Gatherer receives log messages
 type Gatherer struct {
@@ -20,14 +22,24 @@ type Gatherer struct {
 
 // NewGatherer initializes a Gatherer
 func NewGatherer() *Gatherer {
-	return &Gatherer{receiveQueue: make(chan common.LogMessage)}
+	return &Gatherer{receiveQueue: make(chan common.LogMessage, channelBufferSize)}
 }
 
 // Send sends a log message
 func (g *Gatherer) Send(msg common.LogMessage, reply *bool) error {
-	log.Print("send called with", msg)
+	log.Debug("send called with", msg)
 	g.receiveQueue <- msg
-	log.Print("written to queue")
+	log.Debug("message written to queue")
+	return nil
+}
+
+// SendMultiple sends many log messages
+func (g *Gatherer) SendMultiple(msgs []common.LogMessage, reply *bool) error {
+	log.Debug("send called with", msgs)
+	for _, msg := range msgs {
+		g.receiveQueue <- msg
+	}
+	log.Debug(len(msgs), " messages written to queue")
 	return nil
 }
 
@@ -37,21 +49,28 @@ func serve() *Gatherer {
 	rpc.HandleHTTP()
 	l, e := net.Listen(protocol, port)
 	if e != nil {
-		log.Fatal("failed to bind " + port)
+		log.Fatal("failed to bind ", port, e)
 	}
-	log.Print("bound " + port)
+	log.Info("bound ", port)
 	go http.Serve(l, nil)
-	log.Print("serving as http")
+	log.Info("serving as http")
 	return g
 }
 
 func consume(g *Gatherer) {
 	for {
-		log.Print("consumed queue message ", <-g.receiveQueue)
+		log.Debug("consumed queue message ", <-g.receiveQueue)
 	}
 }
 
+func configLogger() {
+	log.SetFormatter(&log.TextFormatter{ForceColors: true})
+	log.SetOutput(colorable.NewColorableStdout())
+	log.SetLevel(log.DebugLevel)
+}
+
 func main() {
+	configLogger()
 	g := serve()
 	consume(g)
 }
--- send/sender.go
+++ send/sender.go
@@ -1,29 +1,56 @@
 package main
 
 import (
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/rpc"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/griever989/gatherlogs/common"
+	"github.com/mattn/go-colorable"
 )
 
 func connect(target string) *rpc.Client {
 	client, err := rpc.DialHTTP("tcp", target)
 	if err != nil {
-		log.Fatal("failed to connect to " + target)
+		log.Fatal("failed to connect to ", target)
 	}
-	log.Print("connected")
+	log.Print("connected to ", target)
 	return client
 }
 
-func main() {
-	client := connect("localhost:9494")
+func configLogger() {
+	log.SetFormatter(&log.TextFormatter{ForceColors: true})
+	log.SetOutput(colorable.NewColorableStdout())
+	log.SetLevel(log.DebugLevel)
+}
+
+func testSend(client *rpc.Client) {
 	var response bool
 	msg := common.LogMessage{Message: "test", Time: &timestamp.Timestamp{}}
 	err := client.Call("Gatherer.Send", &msg, &response)
 	if err != nil {
-		log.Fatal("failed to send")
+		log.Fatal("failed to send ", err)
 	}
-	log.Print("sent", msg)
+	log.Debug("sent", msg)
+}
+
+func testSendMulti(client *rpc.Client) {
+	var response bool
+	msgs := make([]common.LogMessage, 20)
+	msg := common.LogMessage{Message: "test", Time: &timestamp.Timestamp{}}
+	for i := range msgs {
+		msgs[i] = msg
+	}
+	err := client.Call("Gatherer.SendMultiple", &msgs, &response)
+	if err != nil {
+		log.Fatal("failed to send ", err)
+	}
+	log.Debug("sent multiple", msg)
+}
+
+func main() {
+	configLogger()
+	client := connect("localhost:9494")
+	testSend(client)
+	testSendMulti(client)
 }
